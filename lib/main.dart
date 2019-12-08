1184920f19ea2e18c74b0e8d06d0ba616d564420
hamed pishdad <hamed.hpn332@gmail.com>
2019-12-09 02:20:06 +0330

    fix one import path

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'providers/gread_places_p.dart';
+import 'providers/great_places_p.dart';
 import 'screens/places_list_screen.dart';
 import 'screens/add_place_screen.dart';
 
