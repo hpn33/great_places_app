b50dc9d20545979043e7dab8efd960ccde27e6d0
hamed pishdad <hamed.hpn332@gmail.com>
2019-12-11 00:33:06 +0330

    fix map url

--- lib/helper/location_helper.dart
+++ lib/helper/location_helper.dart
@@ -9,9 +9,9 @@ class LocationHelper {
   }) {
     return 'https://maps.googleapis.com/maps/api/staticmap?' +
         'center=' +
-        '&$latitude,$longitude' +
+        '&$altitude,$longitude' +
         '&zoom=16&size=600x300&maptype=roadmap' +
-        '&markers=color:red%7Clabel:A%7C$latitude,$longitude' +
+        '&markers=color:red%7Clabel:A%7C$altitude,$longitude' +
         '&key=$GOOGLE_API_KEY';
   }
 }
